1be3cc9cfb5339b7201e45d9dd482fecf34fe55a
THANYAPHISIT <thanyaphisit.k@pdkm.tech>
2026-02-25 18:13:19 +0700

    claude-switch incorrect url

--- claude-switch.rb
+++ claude-switch.rb
@@ -2,7 +2,7 @@ class ClaudeSwitch < Formula
   desc "A blazingly fast profile manager for Claude Code"
   homepage "https://github.com/THANYAPHISIT1/homebrew-tap"
   
-  url "https://github.com/THANYAPHISIT1/homebrew-tap/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/THANYAPHISIT1/homebrew-tap/releases/download/v0.2.0/claude-switch-mac.tar.gz"
   
   sha256 "51cee78d9d3a7270819c49164fc3384184d1dded3de9f8ac6f569ef83454ffd0"
   
