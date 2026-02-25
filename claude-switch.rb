8fda11f55218a4095bc9e3039da0fb58d9dca943
THANYAPHISIT <thanyaphisit.k@pdkm.tech>
2026-02-25 17:23:56 +0700

    claude-switch

---
+++ claude-switch.rb
@@ -0,0 +1,18 @@
+class ClaudeSwitch < Formula
+  desc "A blazingly fast profile manager for Claude Code"
+  homepage "https://github.com/THANYAPHISIT1/homebrew-tap"
+  
+  url "https://github.com/THANYAPHISIT1/homebrew-tap/releases/download/v0.1.0/claude-switch-mac.tar.gz"
+  
+  sha256 "51cee78d9d3a7270819c49164fc3384184d1dded3de9f8ac6f569ef83454ffd0"
+  
+  license "MIT"
+
+  def install
+    bin.install "claude-switch" 
+  end
+
+  test do
+    system "#{bin}/claude-switch", "--help"
+  end
+end
